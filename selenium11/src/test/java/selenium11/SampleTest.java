fadb46b085ca1138aa3de7fa51c8596141c7e299
suriya99git <suriyalenova99@gmail.com>
2024-01-30 09:40:25 +0530

    selenium11

---
+++ selenium11/src/test/java/selenium11/SampleTest.java
@@ -0,0 +1,13 @@
+package selenium11;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void testCase() {
+		
+		ChromeDriver driver = new ChromeDriver();
+	}
+}
